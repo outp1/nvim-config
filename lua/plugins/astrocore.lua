12c8b65ec0351db69f3a24b64a5f87ad95013043
Daniil Sobolev <dansobol2511@yandex.ru>
2024-12-14 01:59:26 +0300

    migrate some options from old config

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -11,45 +9,39 @@ return {
   ---@type AstroCoreOpts
   opts = {
     -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
+    -- features = {
+    --   large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+    --   autopairs = true, -- enable autopairs at start
+    --   cmp = true, -- enable completion at start
+    --   diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+    --   highlighturl = true, -- highlight URLs at start
+    --   notifications = true, -- enable notifications at start
+    -- },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
+    -- diagnostics = {
+    --   virtual_text = true,
+    --   underline = true,
+    -- },
     -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
-      },
-    },
+    -- options = {
+    --   opt = { -- vim.opt.<key>
+    --     relativenumber = true, -- sets vim.opt.relativenumber
+    --     number = true, -- sets vim.opt.number
+    --     spell = false, -- sets vim.opt.spell
+    --     signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+    --     wrap = false, -- sets vim.opt.wrap
+    --   },
+    -- g = { -- vim.g.<key>
+    --   -- configure global vim variables (vim.g)
+    --   -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+    --   -- This can be found in the `lua/lazy_setup.lua` file
+    -- },
+    -- },
     -- Mappings can be configured through AstroCore as well.
     -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
       -- first key is the mode
       n = {
-        -- second key is the lefthand side of the map
-
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
         -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
           function()
@@ -58,6 +50,27 @@ return {
             )
           end,
           desc = "Close buffer from tabline",
+        },
+
+        ["+"] = {
+          function() require("astrocore.buffer").nav(vim.v.count1) end,
+          desc = "Next buffer",
+        },
+        ["_"] = {
+          function() require("astrocore.buffer").nav(-vim.v.count1) end,
+          desc = "Previous buffer",
+        },
+
+        -- Telescope
+        ["<Leader><Leader>"] = {
+          "<cmd>Telescope frecency workspace=CWD<cr>",
+          desc = "Find files",
+        },
+
+        -- Doge docstrings autogenerate
+        ["<Leader>lg"] = {
+          "<cmd>DogeGenerate<cr>",
+          desc = "Generate docstrings",
         },
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
