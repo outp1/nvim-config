45a73931c8b6228290b16e1eee34b811f9ad1431
Daniil Sobolev <dansobol2511@yandex.ru>
2024-12-14 02:00:00 +0300

    add frecency with telescope instead of find_files

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,44 @@
+return {
+  -- Extensions for Telescope
+  {
+    "nvim-telescope/telescope-frecency.nvim",
+    config = function() require("telescope").load_extension "frecency" end,
+  },
+
+  -- Telescope config
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        -- Default configuration for telescope goes here:
+        -- config_key = value,
+        -- mappings = {
+        --   i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        -- ["<C-h>"] = "which_key",
+        --   }
+        -- }
+        path_display = {
+          filename_first = {
+            reversed_directories = true,
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--sortr=modified" },
+          hidden = true,
+        },
+      },
+      extensions = {
+        -- Your extension configuration goes here:
+        frecency = {
+          matcher = "fuzzy",
+        },
+        -- please take a look at the readme of the extension you want to configure
+      },
+    },
+  },
+}
