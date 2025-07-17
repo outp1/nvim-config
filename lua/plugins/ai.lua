198fd312b2b21b431fa2df96e78435a1ed91e1c5
Daniil Sobolev <dansobol2511@yandex.ru>
2025-07-17 23:52:17 +0300

    add mcp-hub

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,6 +1,16 @@
 ---@type LazySpec
 return {
 
+  -- mcp hub for codecompanion
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    config = function() require("mcphub").setup() end,
+  },
+
   -- codecompanion extensions
   {
     "olimorris/codecompanion.nvim",
@@ -70,6 +80,23 @@ return {
         end,
       },
       extensions = {
+
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            -- MCP Tools
+            make_tools = true, -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
+            show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
+            add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
+            show_result_in_chat = true, -- Show tool results directly in chat buffer
+            format_tool = nil, -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+            -- MCP Resources
+            make_vars = true, -- Convert MCP resources to #variables for prompts
+            -- MCP Prompts
+            make_slash_commands = true, -- Add MCP prompts as /slash commands
+          },
+        },
+
         history = {
           enabled = true,
           opts = {
@@ -131,6 +158,7 @@ return {
             },
           },
         },
+
       },
       prompt_library = {
         ["Project Coordinator"] = {
